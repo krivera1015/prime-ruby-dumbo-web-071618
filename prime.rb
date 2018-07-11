db047ab0b3b3ba0b585652fcd91aad86ec487394
krivera1015 <krivera1015@gmail.com>
2018-07-11 16:08:27 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,12 +3,16 @@
 #if a number is divisible by either 2 3 4 5 and it is 0 it will give non prime number
   #if it is not divisible by those then it is prime
   # all? iterate through array and will see if each follows condition
+  #.to_a - anything to array
 
 def prime?(n)
-  if n < 1
+  if n < 1 
   #if n % 2 == 0 || n % 3 == 0 || n % 4 == 0 || n% 5 == 0
     return false
   else
-    return true
+    (2..n).to_a.all? do |i|
+    n % i != 0
+    end
+    #return true
   end
 end
