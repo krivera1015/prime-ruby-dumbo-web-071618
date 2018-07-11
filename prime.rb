bb733960c71a1c7a49a2e85751437242e3eedcae
krivera1015 <krivera1015@gmail.com>
2018-07-11 15:57:22 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,11 +3,10 @@
 #if a number is divisible by either 2 3 4 5 and it is 0 it will give non prime number
   #if it is not divisible by those then it is prime
 def prime?(n)
-  if n % 2 == 0 || n % 3 == 0 || n % 4 == 0 || n% 5 == 0
+  if n < 0 || n == 0 || n == 1
+  #if n % 2 == 0 || n % 3 == 0 || n % 4 == 0 || n% 5 == 0
     return false
   else
     return true
   end
 end
-
-
